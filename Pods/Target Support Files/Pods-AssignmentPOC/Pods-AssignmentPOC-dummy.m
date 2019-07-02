bcfaf4f35f1af94d42f519ef236a7e6c8a8ea39a
Nishchita <nishchita91@gmail.com>
2019-07-02 21:07:23 +0530

    Pod setup for Alamofire framework

---
+++ Pods/Target Support Files/Pods-AssignmentPOC/Pods-AssignmentPOC-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AssignmentPOC : NSObject
+@end
+@implementation PodsDummy_Pods_AssignmentPOC
+@end
---
+++ Pods/Target Support Files/Pods-AssignmentPOCUITests/Pods-AssignmentPOCUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AssignmentPOCUITests : NSObject
+@end
+@implementation PodsDummy_Pods_AssignmentPOCUITests
+@end
